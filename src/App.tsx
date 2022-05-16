716df42fc283216dd5aac4b938759f4c552656ea
martin-ragan <dev.martin.ragan@gmail.com>
2022-05-16 19:26:51 +0200

    feat: tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen">
+
+      </div>
   );
 }
 
